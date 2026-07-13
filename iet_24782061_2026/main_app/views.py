370107e6adb2014e98cca1c9c15897a19ede0a34
reynaldokurniajayasakti <reynaldokurniajayasakti@gmail.com>
2026-07-13 18:47:43 +0700

    Labsession3

--- iet_24782061_2026/main_app/views.py
+++ iet_24782061_2026/main_app/views.py
@@ -3,7 +3,7 @@ from .models import Report
 from .forms import ReportForm
 
 def home(request):
-    # Logika Simpan (Create)
+    # Logika Simpan Laporan (Create)
     if request.method == "POST":
         form = ReportForm(request.POST)
         if form.is_valid():
@@ -12,22 +12,10 @@ def home(request):
     else:
         form = ReportForm()
 
-    # Logika Tampilkan (Read)
+    # Logika Ambil Data (Read)
     reports = Report.objects.all().order_by('-created_at')
     
-    # Render ke home.html (gabungan Lab 2 & Lab 3)
     return render(request, 'main_app/home.html', {
         'form': form, 
         'reports': reports
-    })
-
-# Opsional: Jika kamu tetap ingin path /add/ sendiri, biarkan fungsi ini ada
-def add_report(request):
-    if request.method == "POST":
-        form = ReportForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    else:
-        form = ReportForm()
-    return render(request, 'main_app/home.html', {'form': form})
+    }
